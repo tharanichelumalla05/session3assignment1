8104217fab61083aa2d37d5fbf3fb9e254fa5fb2
tharanichelumalla05 <31057358+tharanichelumalla05@users.noreply.github.com>
2017-08-31 10:13:04 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/*program for printing hello world*/
+
+package session3;// here I have created a package as session3
+public class HelloWorld {
+	//declares a class called HelloWorld
+	//classes are the basics of opps(object oriented programming)
+	public static void main(String[] args){
+		//TODO Auto-generated method stub
+		//Here public is a access modifier which defines who can access this method
+		//Here static a keyword which identifies class related thing
+		//void is used to define return type of the method,void means method wont return any value
+		//main is name of method
+		//declares method String[]
+		//String[]args means arguments will be passed into main method and says that main() as a parameter
+		System.out.println("hello world");
+		//success
+		//system is used to return code
+		//out is static number
+		//Println is used to print text hello world and gives output
+	}
+
+}
